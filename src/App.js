f243c67ac9cd3ea62139bb02b7e3f01bf30f06de
pmason <pmason@argenx.com>
2023-05-19 07:40:23 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DataGridPro, LicenseInfo } from "@mui/x-data-grid-pro";
+import { Box, Tooltip, Button } from "@mui/material";
+import "./App.css";
 
 function App() {
+  LicenseInfo.setLicenseKey(
+    "5b931c69b031b808de26d5902e04c36fTz00Njk0NyxFPTE2ODg4MDI3MDM3MjAsUz1wcm8sTE09c3Vic2NyaXB0aW9uLEtWPTI="
+  );
+  const cols = [{ field: "info", headerName: "Info", width: 200 }],
+    [rows, setRows] = useState([{ id: 1, info: "123" }]),
+    add = () => {
+      const seconds = Date.now(),
+        tempRows = [...rows, { id: seconds, info: seconds.toString() }];
+      setRows(tempRows);
+    },
+    remove = () => {
+      const tempRows = [...rows];
+      tempRows.pop();
+      setRows(tempRows);
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Example</h1>
+      <Box
+        sx={{
+          backgroundColor: "lightyellow",
+          fontSize: 12,
+          fontWeight: "bold",
+        }}
+      >
+        Here's a MUI Data Grid in a MUI Box
+        <DataGridPro
+          columns={cols}
+          rows={rows}
+          sx={{ backgroundColor: "lightblue", width: 500, height: 400, m: 10 }}
+        />
+        <Tooltip title="add data">
+          <Button onClick={add}>Add</Button>
+        </Tooltip>
+        {rows.length > 5 && (
+          <Tooltip title="remove data">
+            <Button onClick={remove}>Remove</Button>
+          </Tooltip>
+        )}
+      </Box>
+    </>
   );
 }
 
